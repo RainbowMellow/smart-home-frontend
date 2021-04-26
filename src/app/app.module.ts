0ae5bd245544f3183953332b042b56da899290be
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-04-26 15:09:23 +0200

    Setting up bootstrap, state management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgxsModule} from '@ngxs/store';
+import { environment } from 'src/environments/environment';
+import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+    NgbModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production
+    }),
+    NgxsLoggerPluginModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
