2a84aa95694652adec636b7bb4c3f261d0d39374
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-04-27 15:11:30 +0200

    New commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import {NgxsModule} from '@ngxs/store';
+import {SmartItemState} from '../shared/state/smartItem.state';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    NgxsModule.forFeature([SmartItemState])
   ]
 })
 export class HomeModule { }
